a18c1b4a95b3347febc3a7462efd538ce4174b5a
strukovd <dmitryi97@yandex.ru>
2025-04-30 08:41:52 +0600

    eqwewqewq

--- src/main/java/ru/strukovd/eng/services/WordService.java
+++ src/main/java/ru/strukovd/eng/services/WordService.java
@@ -1,13 +1,24 @@
 package ru.strukovd.eng.services;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.strukovd.eng.entities.WordEntity;
+import ru.strukovd.eng.repositories.WordRepository;
+
 import java.util.List;
 
 @Service
 @Getter
+@RequiredArgsConstructor
 public class WordService {
+    private final WordRepository wordRepository;
+
     public List<String> getList() {
-        return List.of("string1", "string2", "string3");
+        List<WordEntity> list = wordRepository.findAll();
+
+        System.out.println(list);
+
+        return list.stream().map(WordEntity::getWord).toList();
     }
 }
